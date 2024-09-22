959354215fb4038b4078b8341a5508448852099a
alexnyeinlabs <acn.alexchan@gmail.com>
2024-09-22 14:45:35 +0700

    1st-commit

---
+++ 1_hello_world.py
@@ -0,0 +1,61 @@
+# function: builtin.py <- library [print()]
+print("Hello World!") # string
+
+# What is 'print()' function?
+# It is a function which takes inputs from user, process it and 
+# send it back to display (console, webapp, app).
+
+print('A') # char
+
+print(25) # int
+
+print(3.142) # float
+
+print(True) # boolean
+
+# What is Expression?
+# Expression is a piece of code that produces a value.
+print('*' * 10) # this is an expression
+
+
+"""
+As you know python code is executed line by line.
+
+What is Method, Function & Library?
+Both method, function and library are program codes. But
+- method(): it is a code that belong to a specific object.
+- function(): it is a code that doesn't belong to a specific object.
+- library: it is a pre-defined files/codes.
+
+What is Data Types?
+A data type is an attribute with a piece of data that tells a computer
+how to interpret a value.
+Algorithms uses variables to store values.
+When an algorithms are converted into programs, the computer needs to be
+told what type of data is stored in each variable.
+
+So, every programming language has a number of built-in data types.
+
+There are 5 general types of data types that can be found in most of the
+programming languages.
+
+- String -> it will store a set of characters that include spaces, tabs,
+alphabets, special characters and even numbers. String is treated as text rather than numbers
+because it is a mixture of alphabets, numbers and special characters.
+
+- Character -> it will store a single character one at a time. It is 
+similar to 'string' that store spaces, tabs, alphabets, speical characters
+and even numbers.
+
+- Integer -> it will only store whole numbers without a fractional/decimal
+part.
+
+- Float, Double, Decimal, Real -> it will store a number with a fractional/decimal
+part.
+
+- Boolean -> it will only one value at a time from two possible values it
+only has.
+
+But in python, we don't have 'character' data type. Python treat the
+character data type as a string data type.
+"""
---
+++ 2_variables.py
@@ -0,0 +1,12 @@
+"""
+In this exercise, we're going to learn about variables which are one of
+the most fundamental concept in most programming languages.
+
+We use variable to temporarily or permenantly store data on the computer
+memory.
+
+What is Variable?
+A variable is a box which stores the data that user gives. The data
+inside a variable is called value. A value inside a variable is not
+fixed and which can be changed during the program is running.
+"""
