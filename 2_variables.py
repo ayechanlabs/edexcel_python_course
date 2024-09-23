b51a692fdb0e9444b2b178208d435d6a245e7869
alexnyeinlabs <acn.alexchan@gmail.com>
2024-09-23 18:37:41 +0700

    2nd-commit

--- 2_variables.py
+++ 2_variables.py
@@ -9,4 +9,56 @@ What is Variable?
 A variable is a box which stores the data that user gives. The data
 inside a variable is called value. A value inside a variable is not
 fixed and which can be changed during the program is running.
+
+In programming, there are two types of a tool that can store the data
+that user gives.
+- variable -> the value its store is not fixed (as long as the program
+runs)
+- constant -> the value its store is fixed (as long as the program runs)
+
+In programming, there are ways to write variable in programs.
+- camel case : firstName, lastName
+- snake case : first_name, last_name
 """
+
+price = 50
+print(price) # 50
+
+# the output is 'price', bcoz instead of calling a variable you write
+# the variable name in string format.
+print('prince')
+
+# Update the value inside a variable
+price = 20
+print(price) # 20
+
+rating = 4.5
+print(rating)
+
+name = 'Alex'
+
+studName = "Aye Chan Nyein"
+
+is_published = True
+
+"""
+In python, we can not only these data types - string, char, int, float
+& boolean but also we can store complex values like list (array) etc.
+
+What is variable initialisation?
+A variable is declared, the computer gives it a location in its memory.
+Initially, the location is empty. but when we gives a value to a variable
+it stores in a memory.
+
+How to initialize a variable?
+In python, we have to give a value to a variable in order to initialize.
+Example: 
+
+var-name    assignment operator     value
+name        =                       'Alex'
+"""
+
+# in here, you will notice that in variable we can various types of data
+# type without declaring the data type.
+# Bcoz, in python we don't have to declare the data type.
+# The python interpreter decide by itself.
---
+++ 3_variable_exercise.py
@@ -0,0 +1,26 @@
+"""
+We check-in a patient named John Smith. He's 20 years old and is a new patient.
+Assign name, age and patient (as boolean) to a variable.
+
+Print a message like this:
+Hello, this is John Smith. He is 35 years old.
+Is he new here: True
+"""
+
+name = 'John Smith'
+age = '20' # change 20 to '20'
+is_new = 'True' # change True to 'True'
+
+print("Hello, this is", name, ". He is", age, "years old.")
+print("Is he new here:", is_new)
+
+print("Hello, this is " + name + ". He is " + age + " years old.")
+print("Is he new here: " + is_new)
+
+"""
+In this exercise, we will learn about different types of string concatenation.
+In python, there are 2 types of string concatenation.
+- using '+' sign, that only concat the string but not other data types.
+- using ',' sign, they will concat any kinds of data types without changing
+the data types and includes extra space.
+"""
